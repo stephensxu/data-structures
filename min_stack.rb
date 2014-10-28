f51d11cc7b1f7fb23748b4de1f6b33f52880dd90
Stephens Xu <stephensxub@gmail.com>
2014-10-27 23:09:39 -0700

    implemented min_stack.rb

--- min_stack.rb
+++ min_stack.rb
@@ -1,21 +1,23 @@
 require_relative 'linked_list'
+require_relative 'stack'
 
 # Implement a "min stack" - a stack that has a contant-time
 # "minimum" operations.
 
 class MinStack < Stack
-  attr_accessor :min
+
+  attr_accessor :min_value, :list
 
   def initialize
-    @min = Float::INFINITY
+    @min_value = Float::INFINITY
     @list = LinkedList.new
     @size = 0
   end
 
   # Places +item+ on the top of the stack
   def push(item)
-    new_min = [item.to_i, self.min]
-    super([item, new_min])
+    @min_value = [item.value.to_i, self.min_value].min
+    @list.unshift([item, @min_value])
   end
 
   # Removes the item on the top of the stack and returns it.
@@ -26,7 +28,7 @@ class MinStack < Stack
 
   # Return the item on the top of the stack without removing it
   def peek
-    super[0]
+    @list.head.value[0].value
   end
 
   # Return true if the stack is empty and false otherwise
@@ -42,6 +44,20 @@ class MinStack < Stack
   # Returns the smallest item on the stack
   # O(1) time
   def min
-    empty? ? Float::INFINITY : @list.head.value[1]
+    empty? ? -Float::INFINITY : @list.head.value[1]
   end
 end
+
+a = MinStack.new
+p a.min
+a.push(Node(1))
+a.push(Node(2))
+a.push(Node(-1))
+a.push(Node(-10))
+a.push(Node(8))
+a.list.each { |item| p item }
+p a.peek
+p a.min
+a.pop
+p a.peek
+p a.min
